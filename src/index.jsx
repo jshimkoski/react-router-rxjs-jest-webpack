d4c3cef4c2bbcb8c282f73b73ddb1b57ba0a5e85
Jason Shimkoski <jshimkoski@me.com>
2016-10-25 06:27:37 -0700

    fixed compiling/intellisence issues

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import TestComponent from 'components/test/test.component'
+
+let test = new TestComponent();
+test.setName("test name");
+
+let test2 = new TestComponent();
+test2.getName();
+
+test2.setName("test 2 name");
+test.getName();
+
 const root = document.createElement('div');
 document.body.appendChild(root);
 

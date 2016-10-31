20468a90897b6fae0b741a7b41dffa461cde838d
Jason Shimkoski <jshimkoski@me.com>
2016-10-30 20:25:18 -0700

    updated homer import name

--- src/components/nav/nav.component.jsx
+++ src/components/nav/nav.component.jsx
@@ -1,13 +1,13 @@
 import React, {Component} from 'react';
 import IndexLink from 'react-router/lib/IndexLink';
 
-import homer from './homer.jpeg';
+import Homer from './homer.jpeg';
 import './nav.scss';
 
 const Nav = props => {
   return (
     <nav className="nav">
-      <img src={homer}/>
+      <img src={Homer}/>
       <ul>
         <li><IndexLink to="home" activeClassName="active">Home</IndexLink></li>
         <li><IndexLink to="about" activeClassName="active">About</IndexLink></li>
